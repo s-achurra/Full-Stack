7c44a847a21f43a7eb5c2711ff20393389c672fc
Stephen Achurra <achurra7@gmail.com>
2017-05-17 15:59:29 -0700

    Setup components for footer

---
+++ frontend/components/footer/footer_component.js
@@ -0,0 +1,13 @@
+import { connect } from 'react-redux';
+import Footer from './footer';
+import { logout } from '../../actions/session_actions';
+
+const mapStateToProps = ({ session }) => ({
+  currentUser: session.currentUser
+});
+
+const mapDispatchToProps = dispatch => ({
+  logout: () => dispatch(logout())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Footer);
