310842ebf26411dcf32f035b25fd0ce3eb8cef58
Stephen Achurra <achurra7@gmail.com>
2017-05-20 14:23:13 -0700

    Fix error with event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ActiveRecord::Base
 
   after_initialize :set_default_image
 
-  def default_image
+  def set_default_image
     self.image_url ||= "https://secure.meetupstatic.com/s/img/041003812446967856280/logo/svg/logo--script.svg"
   end
 end
