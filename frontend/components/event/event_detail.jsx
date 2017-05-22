b1335fce49bd9963a2a3fe142c987ecf7d389fb6
Stephen Achurra <achurra7@gmail.com>
2017-05-22 11:37:54 -0700

    Route to owner group on Event Delete, add link to owner group on Event Detail, add host on Event Detail

--- frontend/components/event/event_detail.jsx
+++ frontend/components/event/event_detail.jsx
@@ -13,8 +13,9 @@ class EventDetail extends React.Component {
 
   handleDelete(e) {
     e.preventDefault();
+    let groupId = this.props.event.group.id
     this.props.deleteEvent(e.target.id)
-      .then(window.location.href="#/events");
+      .then(window.location.href=`#/group/${groupId}`);
   }
 
   render() {
@@ -35,6 +36,12 @@ class EventDetail extends React.Component {
                 <img className="eventImage" src={this.props.event.image_url}/>
                 <h3>Location: </h3>
                 <p>{this.props.event.location}</p>
+                <h3>Group: </h3>
+                <Link to={`/group/${this.props.event.group.id}`} >
+                <p>{this.props.event.group.title}</p>
+                </Link>
+                <h3>Host: </h3>
+                <p>{this.props.event.host.username}</p>
               </section>
 
               <section className="eventDetailRight">
