40991241e83353fd070b8d046c9cc183b5670159
Stephen Achurra <achurra7@gmail.com>
2017-06-15 11:15:37 -0700

    Update logo

--- frontend/components/logo/logo.jsx
+++ frontend/components/logo/logo.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 const Logo = () => (
   <section className="logo">
     <a className="logo" href="/">
-      <img id="logoScript"
-      alt="Meetup home"
-      src="https://secure.meetupstatic.com/s/img/041003812446967856280/logo/svg/logo--script.svg"/>
+      <p>MeetApp</p>
     </a>
   </section>
 );
